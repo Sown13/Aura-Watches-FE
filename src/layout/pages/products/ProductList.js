adabedd6bf9445b031cb96d47f623bee8bad252c
Sown13 <sonnh.buh@gmail.com>
2024-04-12 22:58:52 +0700

    done with setting banner, add badge NEW to ProductCard

--- src/layout/pages/products/ProductList.js
+++ src/layout/pages/products/ProductList.js
@@ -7,23 +7,31 @@ import "../../../css/layout/pages/products/ProductList.css";
 
 export default function ProductList() {
     const { category } = useParams();
+    const { testContext } = useContext(UserContext);
 
     const [productList, setProductList] = useState([]);
-    const { testContext } = useContext(UserContext);
+    const [banner, setBanner] = useState('/img/banner_men.svg');
+
 
     useEffect(() => {
         ProductService.getAllProduct().then((res) => {
             setProductList(res.data);
         }).catch((err) => { console.error("Failed to fetch ", err) });
-    }, [])
+    }, [category])
+
+    useEffect(() => {
+        category ? setBanner(`/img/banner_${category}.svg`) : setBanner('/img/banner_men.svg');
+    }, [category]);
+
 
     return (
         <div className='product-list text-light'>
             <h5 className='product-list-title'> HOME/{category ? category.toLocaleUpperCase() : 'ALL'} </h5>
             TEST: {testContext}
+            <div className='product-list-banner'><img src={banner} alt='BANNER' /></div>
             <div className='row'>
                 {productList.map((product, index) => (
-                    <div className="product-list-card col-lg-3 col-md-4 col-sm-12">
+                    <div className="product-list-card col-lg-3 col-md-4 col-sm-12" key={product.id}>
                         <ProductCard product={product}></ProductCard>
                     </div>
                 ))}
